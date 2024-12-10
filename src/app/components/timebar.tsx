5f2d64ac42aedd0139cf6d6610d067f5fe8a5e08
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-12-11 01:03:55 +0500

    Update timebar.tsx

--- src/app/components/timebar.tsx
+++ src/app/components/timebar.tsx
@@ -11,41 +11,41 @@ const timebar = () => {
             <Image src="/images/mark.png" alt="" height={6} width={20} />
             <h1 className="text-sm">Pick-Up</h1>
           </div>
-          <div className="flex flex-wrap gap-x-2 mt-6">
+          <div className="flex items-center gap-x-2 mt-6 flex-wrap">
             {/* Location */}
-            <div className="flex flex-col justify-between w-full sm:w-auto">
+            <div className="flex flex-col justify-between w-auto">
               <ul>
                 <li className="text-sm">Location</li>
                 <div className="flex items-center gap-x-2">
-                  <li className="text-xs truncate">Select your city</li>
+                  <li className="text-xs">Select your city</li>
                   <Image src="/images/arrow.png" alt="" height={6} width={20} />
                 </div>
               </ul>
             </div>
 
             {/* Vertical Border */}
-            <div className="hidden sm:block border-l-2 border-gray-300 h-[48px] mx-2"></div>
+            <div className="border-l-2 border-gray-300 h-[48px]"></div>
 
             {/* Date */}
-            <div className="flex flex-col justify-between w-full sm:w-auto">
+            <div className="flex flex-col justify-between w-auto">
               <ul>
                 <li className="text-sm">Date</li>
                 <div className="flex items-center gap-x-2">
-                  <li className="text-xs truncate">Select your date</li>
+                  <li className="text-xs">Select your date</li>
                   <Image src="/images/arrow.png" alt="" height={6} width={18} />
                 </div>
               </ul>
             </div>
 
             {/* Vertical Border */}
-            <div className="hidden sm:block border-l-2 border-gray-300 h-[48px] mx-2"></div>
+            <div className="border-l-2 border-gray-300 h-[48px]"></div>
 
             {/* Time */}
-            <div className="flex flex-col justify-between w-full sm:w-auto">
+            <div className="flex flex-col justify-between w-auto">
               <ul>
                 <li className="text-sm">Time</li>
                 <div className="flex items-center gap-x-2">
-                  <li className="text-xs truncate">Select your time</li>
+                  <li className="text-xs">Select your time</li>
                   <Image src="/images/arrow.png" alt="" height={6} width={18} />
                 </div>
               </ul>
@@ -59,41 +59,41 @@ const timebar = () => {
             <Image src="/images/mark.png" alt="" height={6} width={20} />
             <h1 className="text-sm">Drop-Off</h1>
           </div>
-          <div className="flex flex-wrap gap-x-2 mt-6">
+          <div className="flex items-center gap-x-2 mt-6 flex-wrap">
             {/* Location */}
-            <div className="flex flex-col justify-between w-full sm:w-auto">
+            <div className="flex flex-col justify-between w-auto">
               <ul>
                 <li className="text-sm">Location</li>
                 <div className="flex items-center gap-x-2">
-                  <li className="text-xs truncate">Select your city</li>
+                  <li className="text-xs">Select your city</li>
                   <Image src="/images/arrow.png" alt="" height={6} width={20} />
                 </div>
               </ul>
             </div>
 
             {/* Vertical Border */}
-            <div className="hidden sm:block border-l-2 border-gray-300 h-[48px] mx-2"></div>
+            <div className="border-l-2 border-gray-300 h-[48px]"></div>
 
             {/* Date */}
-            <div className="flex flex-col justify-between w-full sm:w-auto">
+            <div className="flex flex-col justify-between w-auto">
               <ul>
                 <li className="text-sm">Date</li>
                 <div className="flex items-center gap-x-2">
-                  <li className="text-xs truncate">Select your date</li>
+                  <li className="text-xs">Select your date</li>
                   <Image src="/images/arrow.png" alt="" height={6} width={18} />
                 </div>
               </ul>
             </div>
 
             {/* Vertical Border */}
-            <div className="hidden sm:block border-l-2 border-gray-300 h-[48px] mx-2"></div>
+            <div className="border-l-2 border-gray-300 h-[48px]"></div>
 
             {/* Time */}
-            <div className="flex flex-col justify-between w-full sm:w-auto">
+            <div className="flex flex-col justify-between w-auto">
               <ul>
                 <li className="text-sm">Time</li>
                 <div className="flex items-center gap-x-2">
-                  <li className="text-xs truncate">Select your time</li>
+                  <li className="text-xs">Select your time</li>
                   <Image src="/images/arrow.png" alt="" height={6} width={18} />
                 </div>
               </ul>
