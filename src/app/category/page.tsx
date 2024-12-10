886f6b77267968249e6b05b05221f606ac61d700
Bismah Naeem <163287902+BismahNaeem@users.noreply.github.com>
2024-12-11 01:18:19 +0500

    Update page.tsx

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -5,19 +5,20 @@ import HeroSection from '../components/herosection';
 
 const Page = () => {
   return (
-    <div className="flex flex-col lg:flex-row gap-6 px-4 lg:px-10">
+    <div className="flex flex-col px-4 lg:px-10">
       {/* Category Section */}
-      <div className="w-full lg:w-1/3">
+      <div className="hidden lg:block w-full lg:w-1/3">
+        {/* Hidden on smaller screens and visible only on large screens */}
         <Category />
       </div>
 
       {/* Timebar Section */}
-      <div className="w-full lg:w-2/3">
+      <div className="w-full lg:w-2/3 mt-4 lg:mt-0">
         <Timebar />
       </div>
 
       {/* Hero Section */}
-      <div className="w-full">
+      <div className="w-full mt-6">
         <HeroSection />
       </div>
     </div>
